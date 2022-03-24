8835b1b7b774fc09e46d0875ba93ab6ecd7adec9
Всеволод <83037776+VsevolodPin@users.noreply.github.com>
2022-03-25 00:17:23 +0300

    Выполнено задание 5

---
+++ SkillBoxTask5/SkillBoxTask5/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Задание 1.
+// Метод разделения строки на слова.
+// Что нужно сделать:
+// Пользователь вводит в консольном приложении длинное предложение. Каждое слово в этом предложении отделено одним пробелом. 
+// Необходимо создать метод, который в качестве входного параметра принимает строковую переменную, а в качестве возвращаемого значения — массив слов. 
+// После вызова данного метода программа вызывает второй метод, который выводит каждое слово в отдельной строке.   
+
+namespace SkillBoxTask5
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст или нажмите Enter для текста по умолчанию");
+            string text = Console.ReadLine();
+            if (text == "" || text == null) text = "Съешь ещё этих мягких французских булок да выпей чаю"; // чтобы не вводить что-то постоянно
+
+            // Вызываем метод разделения на слова
+            string[] words = SplitSomeText(text);
+            PrintText(words);
+            Console.ReadKey();
+        }
+
+        static string[] SplitSomeText(string txt)
+        {
+            return txt.Split(' ');
+        }
+
+        static void PrintText(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\n");
+            }
+        }
+    }
+}
---
+++ SkillBoxTask5/Task2/Task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// Задание 2.
+// Перестановка слов в предложении.
+// Что нужно сделать...
+// Пользователь вводит в программе длинное предложение. Каждое слово раздельно одним пробелом. После ввода пользователь нажимает клавишу Enter. Необходимо создать два метода:
+// первый метод разделяет слова в предложении;
+// второй метод меняет эти слова местами (в обратной последовательности). 
+
+namespace SkillBoxTask5
+{
+    internal class Task2
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст или нажмите Enter для текста по умолчанию");
+            string text = Console.ReadLine();
+            if (text == "" || text == null) text = "Съешь ещё этих мягких французских булок да выпей чаю"; // чтобы не вводить что-то постоянно
+
+            // Вызываем метод написания слов задом-наперед
+            ReverseWords(text);
+            Console.ReadKey();
+        }
+
+        static void ReverseWords(string txt)
+        {
+            string[] words = SplitSomeText(txt);
+            for (int i = 0; i < words.Length / 2; i++)
+            {
+                string buf = words[i];
+                words[i] = words[words.Length - 1 - i];
+                words[words.Length - 1 - i] = buf;
+            }
+            PrintText(words);
+        }
+
+        static string[] SplitSomeText(string txt)
+        {
+            return txt.Split(' ');
+        }
+
+        static void PrintText(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\n");
+            }
+        }
+    }
+}
