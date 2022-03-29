b954c5e31cd2f9c2c3107aaabd99b3c1713139ed
Всеволод <83037776+VsevolodPin@users.noreply.github.com>
2022-03-29 20:52:43 +0300

    Выполнено задание 6

---
+++ SkillBoxTask6/SkillBoxTask6/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace SkillBoxTask6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.NameTB = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.Patronymic = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.Age = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.HeightTB = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.DateOfBorn = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.PlaceOfBorn = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Surname = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.FileOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.AddWorker = new System.Windows.Forms.Button();
+            this.ReadFile = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FileName);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.NameTB);
+            this.groupBox1.Controls.Add(this.textBox14);
+            this.groupBox1.Controls.Add(this.Patronymic);
+            this.groupBox1.Controls.Add(this.textBox12);
+            this.groupBox1.Controls.Add(this.Age);
+            this.groupBox1.Controls.Add(this.textBox10);
+            this.groupBox1.Controls.Add(this.HeightTB);
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.DateOfBorn);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.PlaceOfBorn);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.Surname);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 256);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление сотрудника";
+            // 
+            // FileName
+            // 
+            this.FileName.Location = new System.Drawing.Point(122, 225);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(128, 23);
+            this.FileName.TabIndex = 16;
+            this.FileName.Text = "Отдел продаж.txt";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 225);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(110, 23);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "Имя файла:";
+            // 
+            // NameTB
+            // 
+            this.NameTB.Location = new System.Drawing.Point(122, 51);
+            this.NameTB.Name = "NameTB";
+            this.NameTB.Size = new System.Drawing.Size(128, 23);
+            this.NameTB.TabIndex = 14;
+            this.NameTB.Text = "Всеволод";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(6, 51);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(110, 23);
+            this.textBox14.TabIndex = 13;
+            this.textBox14.Text = "Имя:";
+            // 
+            // Patronymic
+            // 
+            this.Patronymic.Location = new System.Drawing.Point(122, 80);
+            this.Patronymic.Name = "Patronymic";
+            this.Patronymic.Size = new System.Drawing.Size(128, 23);
+            this.Patronymic.TabIndex = 12;
+            this.Patronymic.Text = "Романович";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(6, 80);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(110, 23);
+            this.textBox12.TabIndex = 11;
+            this.textBox12.Text = "Отчество:";
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(122, 109);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(128, 23);
+            this.Age.TabIndex = 10;
+            this.Age.Text = "23";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(6, 109);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(110, 23);
+            this.textBox10.TabIndex = 9;
+            this.textBox10.Text = "Возраст:";
+            // 
+            // HeightTB
+            // 
+            this.HeightTB.Location = new System.Drawing.Point(122, 138);
+            this.HeightTB.Name = "HeightTB";
+            this.HeightTB.Size = new System.Drawing.Size(128, 23);
+            this.HeightTB.TabIndex = 8;
+            this.HeightTB.Text = "182";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(6, 138);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(110, 23);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.Text = "Рост (см):";
+            // 
+            // DateOfBorn
+            // 
+            this.DateOfBorn.Location = new System.Drawing.Point(122, 167);
+            this.DateOfBorn.Name = "DateOfBorn";
+            this.DateOfBorn.Size = new System.Drawing.Size(128, 23);
+            this.DateOfBorn.TabIndex = 6;
+            this.DateOfBorn.Text = "08.01.1999";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 167);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(110, 23);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "Дата рождения:";
+            // 
+            // PlaceOfBorn
+            // 
+            this.PlaceOfBorn.Location = new System.Drawing.Point(122, 196);
+            this.PlaceOfBorn.Name = "PlaceOfBorn";
+            this.PlaceOfBorn.Size = new System.Drawing.Size(128, 23);
+            this.PlaceOfBorn.TabIndex = 4;
+            this.PlaceOfBorn.Text = "г. Нижний Новгород";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 196);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(110, 23);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Место рождения:";
+            // 
+            // Surname
+            // 
+            this.Surname.Location = new System.Drawing.Point(122, 22);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(128, 23);
+            this.Surname.TabIndex = 2;
+            this.Surname.Text = "Пинчук";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(110, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Фамилия:";
+            // 
+            // FileOutput
+            // 
+            this.FileOutput.Location = new System.Drawing.Point(6, 20);
+            this.FileOutput.Name = "FileOutput";
+            this.FileOutput.ReadOnly = true;
+            this.FileOutput.Size = new System.Drawing.Size(524, 199);
+            this.FileOutput.TabIndex = 1;
+            this.FileOutput.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.FileOutput);
+            this.groupBox2.Location = new System.Drawing.Point(278, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(536, 228);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Чтение файла";
+            // 
+            // AddWorker
+            // 
+            this.AddWorker.Location = new System.Drawing.Point(540, 246);
+            this.AddWorker.Name = "AddWorker";
+            this.AddWorker.Size = new System.Drawing.Size(134, 25);
+            this.AddWorker.TabIndex = 16;
+            this.AddWorker.Text = "Добавить сотрудника";
+            this.AddWorker.UseVisualStyleBackColor = true;
+            this.AddWorker.Click += new System.EventHandler(this.AddWorker_Click);
+            // 
+            // ReadFile
+            // 
+            this.ReadFile.Location = new System.Drawing.Point(680, 246);
+            this.ReadFile.Name = "ReadFile";
+            this.ReadFile.Size = new System.Drawing.Size(134, 25);
+            this.ReadFile.TabIndex = 17;
+            this.ReadFile.Text = "Прочитать файл";
+            this.ReadFile.UseVisualStyleBackColor = true;
+            this.ReadFile.Click += new System.EventHandler(this.ReadFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 280);
+            this.Controls.Add(this.ReadFile);
+            this.Controls.Add(this.AddWorker);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Менеджер сотрудников";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox FileName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox NameTB;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox Patronymic;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox Age;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox HeightTB;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox DateOfBorn;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox PlaceOfBorn;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox Surname;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox FileOutput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button AddWorker;
+        private System.Windows.Forms.Button ReadFile;
+    }
+}
---
+++ SkillBoxTask6/SkillBoxTask6/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkillBoxTask6
+{
+    public partial class Form1 : Form
+    {
+        // Для гибкости
+        private char sep = '|';
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddWorker_Click(object sender, EventArgs e)
+        {
+            string current_time = DateTime.Now.ToString("dd.MM.yyyy hh:mm");
+            string FullName = $"{Surname.Text} {NameTB.Text} {Patronymic.Text}";
+
+            if (File.Exists(FileName.Text))
+            {
+                string[] strs = File.ReadAllLines(FileName.Text);
+                int last_idx = strs.Length - 1;
+                int first_sep_idx = strs[last_idx].IndexOf(sep);
+
+                int last_id = Convert.ToInt16(strs[last_idx].Remove(first_sep_idx));
+                string to_add = $"{last_id + 1}{sep}{current_time}{sep}{FullName}{sep}{Age.Text}{sep}{HeightTB.Text}{sep}{DateOfBorn.Text}{sep}{PlaceOfBorn.Text}";
+                File.AppendAllText(FileName.Text, $"{to_add}\n");
+            }
+            else
+            {
+                string to_add = $"1{sep}{current_time}{sep}{FullName}{sep}{Age.Text}{sep}{HeightTB.Text}{sep}{DateOfBorn.Text}{sep}{PlaceOfBorn.Text}";
+                File.WriteAllText(FileName.Text, $"{to_add}\n");
+            }
+        }
+
+        private void ReadFile_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(FileName.Text))
+            {
+                FileOutput.Text = "";
+                string[] text = File.ReadAllLines(FileName.Text);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    FileOutput.Text += $"{text[i].Replace(sep, ' ')}\n";
+                }
+            }
+            else
+            {
+                FileOutput.Text = "File isn`t exist.";
+            }
+        }
+    }
+}
